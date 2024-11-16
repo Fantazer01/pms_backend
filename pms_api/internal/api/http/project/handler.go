19dca6023b99c08b006e1d554caff1b232a72f36
Oleg Krygin <olegkrygin9@gmail.com>
2024-11-16 18:25:31 +0300

    move "api" to "api/http"

---
+++ pms_api/internal/api/http/project/handler.go
@@ -0,0 +1,160 @@
+package project
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+)
+
+type handler struct {
+}
+
+func NewHandler() *handler {
+	return &handler{}
+}
+
+// GetProjects
+// @Tags Project
+// @Summary Get projects
+// @Description Get projects
+// @Accept json
+// @Produce json
+// @Success 200 {object} model.ProjectsPaged
+// @Router /projects [get]
+func (h *handler) GetProjects(c echo.Context) error {
+	return c.JSON(http.StatusNotImplemented, nil)
+}
+
+// GetProjectByID
+// @Tags Project
+// @Summary Get project by ID
+// @Description Get project by ID
+// @Accept json
+// @Produce json
+// @Success 200 {object} model.Project
+// @Router /projects/{id} [get]
+func (h *handler) GetProjectByID(c echo.Context) error {
+	return c.JSON(http.StatusNotImplemented, nil)
+}
+
+// CreateProject
+// @Tags Project
+// @Summary Create project
+// @Description Create project
+// @Accept json
+// @Produce json
+// @Success 201 {object} model.Project
+// @Router /projects [post]
+func (h *handler) CreateProject(c echo.Context) error {
+	return c.JSON(http.StatusNotImplemented, nil)
+}
+
+// UpdateProject
+// @Tags Project
+// @Summary Update project
+// @Description Update project
+// @Accept json
+// @Produce json
+// @Success 200 {object} model.Project
+// @Router /projects/{id} [put]
+func (h *handler) UpdateProject(c echo.Context) error {
+	return c.JSON(http.StatusNotImplemented, nil)
+}
+
+// DeleteProject
+// @Tags Project
+// @Summary Delete project
+// @Description Delete project
+// @Accept  json
+// @Produce  json
+// @Success 204
+// @Failure 400
+// @Router /projects/{id} [delete]
+func (h *handler) DeleteProject(c echo.Context) error {
+	return c.JSON(http.StatusNotImplemented, nil)
+}
+
+// GetArchivedProjects
+// @Tags Project
+// @Summary Get archived projects
+// @Description Get archived projects
+// @Produce json
+// @Success 200 {object} model.ProjectsPaged
+// @Router /projects/archived [get]
+func (h *handler) GetArchivedProjects(c echo.Context) error {
+	return c.JSON(http.StatusNotImplemented, "")
+}
+
+// ArchiveProject
+// @Tags Project
+// @Summary Archive the project by id
+// @Description Archive the project by id
+// @Produce json
+// @Param project_id path string true "Project id"
+// @Success 200 {object} model.ProjectsPaged
+// @Router /projects/{project_id}/archive [put]
+func (h *handler) ArchiveProject(c echo.Context) error {
+	return c.JSON(http.StatusNotImplemented, "")
+}
+
+// UnarchiveProject
+// @Tags Project
+// @Summary Unarchive the project by id
+// @Description Unarchive the project by id
+// @Produce json
+// @Param project_id path string true "Project id"
+// @Success 200 {object} model.ProjectsPaged
+// @Router /projects/{project_id}/unarchive [put]
+func (h *handler) UnarchiveProject(c echo.Context) error {
+	return c.JSON(http.StatusNotImplemented, "")
+}
+
+// GetProjectMembers
+// @Tags Project
+// @Summary Get project members
+// @Description Get project members
+// @Accept json
+// @Produce json
+// @Success 200 {object} model.UsersPaged
+// @Router /projects/{id}/members [get]
+func (h *handler) GetProjectMembers(c echo.Context) error {
+	return c.JSON(http.StatusNotImplemented, nil)
+}
+
+// AddProjectMember
+// @Tags Project
+// @Summary Add project member
+// @Description Add project member
+// @Accept json
+// @Produce json
+// @Success 200
+// @Failure 400
+// @Router /projects/{project_id}/members/{user_id} [post]
+func (h *handler) AddProjectMember(c echo.Context) error {
+	return c.JSON(http.StatusNotImplemented, nil)
+}
+
+// DeleteProjectMember
+// @Tags Project
+// @Summary Delete project member
+// @Description Delete project member
+// @Accept json
+// @Produce json
+// @Success 204
+// @Failure 400
+// @Router /projects/{id}/members/{user_id} [delete]
+func (h *handler) DeleteProjectMember(c echo.Context) error {
+	return c.JSON(http.StatusNotImplemented, nil)
+}
+
+// GetProjectTasks
+// @Tags Project
+// @Summary Get project tasks
+// @Description Get project tasks
+// @Accept json
+// @Produce json
+// @Success 200 {object} model.Tasks
+// @Router /projects/{id}/tasks [get]
+func (h *handler) GetProjectTasks(c echo.Context) error {
+	return c.JSON(http.StatusNotImplemented, nil)
+}
---
+++ pms_api/internal/api/http/project/route.go
@@ -0,0 +1,24 @@
+package project
+
+import "github.com/labstack/echo/v4"
+
+func (h *handler) RegirterRoutes(router *echo.Group) {
+	projects := router.Group("/projects")
+	{
+		projects.GET("", h.GetProjects)
+		projects.GET("/:id", h.GetProjectByID)
+		projects.POST("", h.CreateProject)
+		projects.PUT("/:id", h.UpdateProject)
+		projects.DELETE("/:id", h.DeleteProject)
+
+		projects.GET("/archived", h.GetArchivedProjects)
+		projects.PUT("/:project_id/archive", h.ArchiveProject)
+		projects.PUT("/:project_id/unarchive", h.UnarchiveProject)
+
+		projects.GET("/:project_id/members", h.GetProjectMembers)
+		projects.POST("/:project_id/members/:user_id", h.AddProjectMember)
+		projects.DELETE("/:project_id/members/:user_id", h.DeleteProjectMember)
+
+		projects.GET("/:project_id/tasks", h.GetProjectTasks)
+	}
+}
