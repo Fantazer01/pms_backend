f5bc5164d6bf0585eed6f6cf065b2e878cbefa9b
Oleg Krygin <olegkrygin9@gmail.com>
2024-11-30 07:33:15 +0300

    add Short model for project and change field for projectPaged

--- pms_api/internal/pkg/model/project.go
+++ pms_api/internal/pkg/model/project.go
@@ -10,14 +10,19 @@ type Project struct {
 	UpdatedAt   time.Time `json:"updated_at"`
 }
 
+type ProjectShort struct {
+	ID   string `json:"id"`
+	Name string `json:"name"`
+}
+
 type InsertProject struct {
 	Name        string `json:"name"`
 	Description string `json:"description"`
 }
 
 type ProjectsPaged struct {
-	PageIndex int        `json:"page_index"`
-	PageSize  int        `json:"page_size"`
-	Total     int        `json:"total"`
-	Projects  []*Project `json:"items"`
+	PageIndex int             `json:"page_index"`
+	PageSize  int             `json:"page_size"`
+	Total     int             `json:"total"`
+	Projects  []*ProjectShort `json:"items"`
 }
