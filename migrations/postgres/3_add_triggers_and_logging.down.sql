f740c32f9b23d7c2a1877f4d030afc90671fe7e5
Yurij <yurij.morozov2002@mail.ru>
2024-12-07 21:27:05 +0300

    Add logging and triggers

---
+++ migrations/postgres/3_add_triggers_and_logging.down.sql
@@ -0,0 +1,14 @@
+DROP TRIGGER IF EXISTS trigger_before_operation_participants_project ON public.participants_project;
+DROP TRIGGER IF EXISTS trigger_after_operation_participants_project ON public.participants_project;
+DROP TRIGGER IF EXISTS trigger_before_operation_role ON public.role;
+DROP TRIGGER IF EXISTS trigger_after_operation_role ON public.role;
+DROP TRIGGER IF EXISTS trigger_before_operation_user_app ON public.user_app;
+DROP TRIGGER IF EXISTS trigger_after_operation_user_app ON public.user_app;
+DROP TRIGGER IF EXISTS trigger_before_operation_project ON public.project;
+DROP TRIGGER IF EXISTS trigger_after_operation_project ON public.project;
+DROP TRIGGER IF EXISTS trigger_before_operation_task ON public.task;
+DROP TRIGGER IF EXISTS trigger_after_operation_task ON public.task;
+DROP FUNCTION IF EXISTS public.before_operation_function;
+DROP FUNCTION IF EXISTS public.after_operation_function;
+DROP TABLE IF EXISTS public.log_changes;
+DROP SEQUENCE IF EXISTS public.log_changes__log_id_seq;
