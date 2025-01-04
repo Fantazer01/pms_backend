0c5e90610cf8e4b8f9d26913e262c7f651d77a17
Oleg Krygin <olegkrygin9@gmail.com>
2025-01-04 14:52:32 +0300

    change type for fields CreatedAt, UpdatedAt

--- pms_api/internal/pkg/model/task.go
+++ pms_api/internal/pkg/model/task.go
@@ -1,13 +1,15 @@
 package model
 
+import "time"
+
 type Task struct {
-	ID          string `json:"id"`
-	Name        string `json:"name"`
-	Description string `json:"description"`
-	ProjectID   string `json:"project_id"`
-	UserID      string `json:"user_id"`
-	CreatedAt   string `json:"created_at"`
-	UpdatedAt   string `json:"updated_at"`
+	ID          string    `json:"id"`
+	Name        string    `json:"name"`
+	Description string    `json:"description"`
+	ProjectID   string    `json:"project_id"`
+	UserID      string    `json:"user_id"`
+	CreatedAt   time.Time `json:"created_at"`
+	UpdatedAt   time.Time `json:"updated_at"`
 }
 
 type TaskInserted struct {
