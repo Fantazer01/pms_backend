da150624c189ff10aebf7f84d922a7bdd49b28e5
Oleg Krygin <olegkrygin9@gmail.com>
2025-01-05 13:46:53 +0300

    realize method get project task and add field "is_active"

--- pms_api/internal/pkg/model/project.go
+++ pms_api/internal/pkg/model/project.go
@@ -6,6 +6,7 @@ type Project struct {
 	ID          string    `json:"id"`
 	Name        string    `json:"name"`
 	Description string    `json:"description"`
+	IsActive    bool      `json:"is_active"`
 	CreatedAt   time.Time `json:"created_at"`
 	UpdatedAt   time.Time `json:"updated_at"`
 }
--- pms_api/internal/repository/project/postgres/model.go
+++ pms_api/internal/repository/project/postgres/model.go
@@ -9,6 +9,7 @@ type project struct {
 	ID          string    `db:"id"`
 	Name        string    `db:"name"`
 	Description string    `db:"description"`
+	IsActive    bool      `db:"is_active"`
 	CreatedAt   time.Time `db:"created_at"`
 	UpdatedAt   time.Time `db:"updated_at"`
 }
@@ -29,6 +30,7 @@ func toProjectFromDb(projectFromDb project) *model.Project {
 		ID:          projectFromDb.ID,
 		Name:        projectFromDb.Name,
 		Description: projectFromDb.Description,
+		IsActive:    projectFromDb.IsActive,
 		CreatedAt:   projectFromDb.CreatedAt,
 		UpdatedAt:   projectFromDb.UpdatedAt,
 	}
@@ -37,7 +39,7 @@ func toProjectFromDb(projectFromDb project) *model.Project {
 type user struct {
 	ID       string `db:"id"`
 	Username string `db:"username"`
-	FullName string `json:"full_name"`
+	FullName string `db:"full_name"`
 }
 
 func toUserShortsFromRepo(usersFromDb []user) []*model.UserShort {
@@ -49,4 +51,36 @@ func toUserShortsFromRepo(usersFromDb []user) []*model.UserShort {
 		}
 	}
 	return users
+}
+
+type task struct {
+	ID          string    `db:"id"`
+	Name        string    `db:"name"`
+	Description string    `db:"description"`
+	Status      string    `db:"status"`
+	ProjectID   string    `db:"project_id"`
+	AuthorID    string    `db:"author_id"`
+	ExecutorID  string    `db:"executor_id"`
+	TesterID    string    `db:"tester_id"`
+	CreatedAt   time.Time `db:"created_at"`
+	Deadline    time.Time `db:"deadline"`
+}
+
+func toTasksFromRepo(tasksFromDb []task) []*model.Task {
+	tasks := make([]*model.Task, len(tasksFromDb))
+	for i := range tasks {
+		tasks[i] = &model.Task{
+			ID:          tasksFromDb[i].ID,
+			Name:        tasksFromDb[i].Name,
+			Description: tasksFromDb[i].Description,
+			Status:      tasksFromDb[i].Status,
+			ProjectID:   tasksFromDb[i].ProjectID,
+			AuthorID:    tasksFromDb[i].AuthorID,
+			ExecutorID:  tasksFromDb[i].ExecutorID,
+			TesterID:    tasksFromDb[i].TesterID,
+			CreatedAt:   tasksFromDb[i].CreatedAt,
+			Deadline:    tasksFromDb[i].Deadline,
+		}
+	}
+	return tasks
 }
